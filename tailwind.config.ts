cde4578aaa363b26171a790c1c1242b87238e6bf
Naga Sheshadri <nagasheshadri001@gmail.com>
2025-07-12 17:35:15 +0000

    use Helvetica Pro Textbook Bold for headline Elastik Regular A for body

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Roboto', 'sans-serif'],
+        headline: ['Montserrat', 'sans-serif'],
       },
       colors: {
         border: "hsl(var(--border))",
