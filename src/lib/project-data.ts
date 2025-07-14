93f5d58d98a656ea20edb37a44e66d1181b4a041
nagasheshadri01 <nagasheshadrikalli@gmail.com>
2025-07-14 11:00:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/lib/project-data.ts
+++ src/lib/project-data.ts
@@ -3,7 +3,7 @@ export const projects = [
   {
     title: "Student Companion",
     description: "An all-in-one app for students to manage schedules, assignments, and notes, enhancing productivity and organization.",
-    image: "https://placehold.co/1200x800.png",
+    image: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxwcm9qZWN0fGVufDB8fHx8MTc1MjM5MjkyMnww&ixlib=rb-4.1.0&q=80&w=1080",
     tags: ["Flutter", "Firebase", "Dart", "GetX"],
     liveLink: "#",
     githubLink: "#",
